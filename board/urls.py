f471d9ad2ca547309725203ca0282e8169e68fec
yiseul826 <cjswothsuis@naver.com>
2020-11-09 14:59:07 +0900

    템플릿 설정 및 테스트

--- board/urls.py
+++ board/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index),
+    # url패턴, 설정파일명.함수명
+    path('', views.index),
+
+    path('list', views.list),
+    path('view', views.view),
+    path('write', views.write),
+    path('update', views.update),
+    path('delete', views.delete),
+
 ]
--- board/views.py
+++ board/views.py
@@ -2,5 +2,20 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, Django Board!!")
+    return render(request, 'index.html')
+
+def list(request):
+    return render(request, 'board/list.html')
+
+def view(request):
+    return render(request, 'board/view.html')
+
+def write(request):
+    return render(request, 'board/write.html')
+
+def update(request):
+    return render(request, 'board/update.html')
+
+def select(request):
+    return render(request, 'board/select.html')
 
